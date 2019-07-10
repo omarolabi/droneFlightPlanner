fe29c1006834f852ea51461e9ffbb7cd7b05f4e8
oolabi <oolabi@stratio.com>
2019-07-10 18:15:41 +0200

    integrating angular google maps

--- src/app/map/map.module.ts
+++ src/app/map/map.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MapComponent } from './map.component';
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [MapComponent],
   exports: [MapComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyA6N6kbJZRlp7_GCYCwS1xsreRjcbY1Qus',
+      libraries: ['places']
+    })
+  ],
   providers: []
 })
 export class MapModule {}
